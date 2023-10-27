8506183454728d7d28f55224f15a401d6f786581
AlejandroSuero <asuerome@gmail.com>
2023-10-27 21:20:50 +0200

    Added head tag

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
 import Link from "next/link"
+import Head from "next/head"
 
-import { Children, type FC, type ReactNode } from "react"
+import type { FC, ReactNode } from "react"
 
 interface AuthProps {
   title: string
@@ -21,43 +22,47 @@ const Auth: FC<AuthProps> = ({
   link,
   children
 }) => {
-  let nextTabIndex = Children.count(children) + 1
   return (
-    <div className="relative h-screen w-screen bg-black lg:bg-[url('/images/hero.jpg')] lg:bg-no-repeat lg:bg-center lg:bg-fixed lg:bg-cover">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <nav className="px-12 py-16">
-          <Link href="/">
-            <Image src="/images/logo.png" alt="Netflix logo" width={167} height={45} />
-          </Link>
-        </nav>
-        <div className="flex justify-center">
-          <main className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-xl w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">
-              {title}
-            </h2>
-            <section className="flex flex-col gap-4">
-              {children}
-            </section>
-            <button
-              tabIndex={nextTabIndex++}
-              className="bg-red-700 py-3 text-white font-bold tracking-wide rounded-md w-full mt-10 hover:bg-red-800 transition focus:outline-none focus:bg-red-800"
-            >
-              {button}
-            </button>
-            <p className="text-neutral-400 mt-12">
-              {footer.text}
-              <Link
-                tabIndex={nextTabIndex++}
-                href={link}
-                className="text-white opacity-[0.85] ml-1 font-semibold cursor-pointer hover:underline hover:opacity-100 focus:opacity-100 focus:outline-none focus:underline"
+    <>
+      <Head>
+        <title>{`Netflix clone - ${title}`}</title>
+        <meta property="og:title" key="title" name="title" content={`Netflix clone - ${title}`}></meta>
+        <meta property="og:description" key="descrition" name="description" content={`Authentication page in which you can ${title.toLowerCase()} and enjoy our movies catalog`} />
+      </Head>
+      <div className="relative h-screen w-screen lg:bg-[url('/images/hero.jpg')] lg:bg-no-repeat lg:bg-center lg:bg-fixed lg:bg-cover">
+        <div className="bg-black w-full h-full lg:bg-opacity-50 flex flex-col">
+          <nav className="px-16 pt-10">
+            <Link href="/" className="block w-fit" title="Go back home">
+              <Image src="/images/logo.png" alt="Netflix logo" width={167} height={45} quality={100} loading="eager" priority />
+            </Link>
+          </nav>
+          <div className="flex-1 flex justify-center self-center w-full h-full">
+            <main className="bg-black bg-opacity-70 px-16 py-16 self-center lg:w-2/5 lg:max-w-md rounded-xl w-full">
+              <h2 className="text-white text-4xl mb-8 font-semibold">
+                {title}
+              </h2>
+              <section className="flex flex-col gap-4">
+                {children}
+              </section>
+              <button
+                className="bg-red-700 py-3 text-white font-bold tracking-wide rounded-md w-full mt-10 hover:bg-red-800 transition focus:outline-none focus:bg-red-800"
               >
-                {footer.link}
-              </Link>
-            </p>
-          </main>
+                {button}
+              </button>
+              <p className="text-neutral-400 mt-12">
+                {footer.text}
+                <Link
+                  href={link}
+                  className="text-white opacity-[0.85] ml-1 font-semibold cursor-pointer hover:underline hover:opacity-100 focus:opacity-100 focus:outline-none focus:underline"
+                >
+                  {footer.link}
+                </Link>
+              </p>
+            </main>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
