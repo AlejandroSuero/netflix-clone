7f83575a003d775721e456f9041ac178f1e6f1c7
AlejandroSuero <asuerome@gmail.com>
2023-10-28 02:49:00 +0200

    Implemented prismadb client

---
+++ lib/prismadb.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client"
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prismadb: PrismaClientSingleton | undefined
+}
+
+const client = globalForPrisma.prismadb ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV === "production") globalForPrisma.prismadb = client
+
+export default client
